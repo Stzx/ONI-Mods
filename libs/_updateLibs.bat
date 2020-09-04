778c025cf20e30a5f9469c5381cb32a0bbff5433
Stzx <silence.m@hotmail.com>
2020-09-04 21:57:54 +0800

    Remove the Json library dependency of NuGet and replace it with the library that comes with Game

--- libs/_updateLibs.bat
+++ libs/_updateLibs.bat
@@ -5,3 +5,4 @@ set PATH=%ONI_PATH%\OxygenNotIncluded_Data\Managed
 xcopy "%PATH%\Unity*.dll" %~dp0 /Y
 xcopy "%PATH%\Assembly-CSharp*.dll" %~dp0 /Y
 xcopy "%PATH%\0Harmony.dll" %~dp0 /Y
+xcopy "%PATH%\Newtonsoft.Json.dll" %~dp0 /Y
