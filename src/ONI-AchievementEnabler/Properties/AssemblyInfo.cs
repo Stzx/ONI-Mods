1e3314b9df1bc210e8b38ac071ee1a8f405957df
Stzx <silence.m@hotmail.com>
2020-12-20 19:43:13 +0800

    Bump 2020.12.20 / CS-444111

--- src/ONI-AchievementEnabler/Properties/AssemblyInfo.cs
+++ src/ONI-AchievementEnabler/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2020.9.4.0")]
+[assembly: AssemblyFileVersion("2020.12.20.0")]
