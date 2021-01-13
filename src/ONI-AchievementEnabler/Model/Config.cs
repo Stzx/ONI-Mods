607feb6b3c37b07cd769a04d87aad4ae310031cd
Stzx <silence.m@hotmail.com>
2021-01-13 21:59:18 +0800

    Update copyright

--- src/ONI-AchievementEnabler/Model/Config.cs
+++ src/ONI-AchievementEnabler/Model/Config.cs
@@ -1,5 +1,5 @@
-﻿/**
- * Copyright (c) 2019-2020 Silence Tai
+/**
+ * Copyright (c) 2019-2021 Silence Tai
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
